e568aefb6155e458910b2ea00aa085e928371456
Marie-LH <marielaurehanriot@gmail.com>
2024-09-05 14:19:06 +0200

    Modification base et home, ajout css, correction controller

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,11 +3,12 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
+
 
 class MainController extends AbstractController
 {
-    #[Route('/', name: 'main-home', methods: ['GET', 'POST'])]
+    #[Route('/', name: 'main_home', methods: ['GET', 'POST'])]
     public function home() {
         return $this->render('main/home.html.twig');
     }
