627c63f95666c6865b6784eff5a5b54b9cba9791
Marie-LH <marielaurehanriot@gmail.com>
2024-09-09 15:29:03 +0200

    Création BDD, création entité wish, modif des pages list et detail

--- src/Controller/WishController.php
+++ src/Controller/WishController.php
@@ -1,25 +1,32 @@
 <?php
 
 namespace App\Controller;
-
+use App\Entity\Wish;
+use App\Repository\WishRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/wish', name: 'wish_', methods: ['GET', 'POST'])]
+#[Route('/wish', name: 'wish_')]
 class WishController extends AbstractController
 {
-    #[Route('', name: 'list')]
-    public function list(): Response
+
+    #[Route('', name: 'list', methods: ['GET'])]
+    public function list(WishRepository $wishRepository): Response
     {
-        //todo affichera la liste des choses à faire
-        return $this->render('list.html.twig');
+        $wish = $wishRepository->findBy(
+            ['isPublished' => 1],    // Filtre : uniquement les éléments publiés (isPublished = 1)
+            ['dateCreated' => 'DESC'] // Tri : du plus récent au plus ancien
+        );
+        return $this->render('list.html.twig', ['wish' => $wish]);
+
     }
 
-    #[Route('/detail/{id}', name: 'detail', requirements: ['id' => '\d+'])]
-    public function detail(int $id): Response
+    #[Route('/detail/{id}', name: 'detail', requirements: ['id' => '\d+'], methods: ['GET'])]
+    public function detail(Wish $wish): Response
     {
         //todo affichera les détails d'une idée de la liste
-        return $this->render('detail.html.twig');
+        return $this->render('detail.html.twig', ['wish' => $wish]);
     }
 }
